65ea6b183d694650721d937afc395572c912d1ed
Dilushkumar Vasudevan <dilushvasu1996@gmail.com>
2026-08-30 12:05:31 +0530

    Added cors library to coomunicate with frontend requests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors());
 const port = 8080;
 
 // Define a route for GET requests to the root URL
 app.get('/', (req : Request, res : Response) => {
   res.send('Hello World from Express!');
+});
+
+app.post('/api/register', (req : Request, res : Response) => {
+  res.json({ message: 'New User Created successfully' });
+  res.status(201);
 });
 
 // Start the server
